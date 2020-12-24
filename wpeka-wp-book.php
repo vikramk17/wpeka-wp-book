6820e65843949210d411d2441deb3344c5f54d33
vikramk17 <vickramskadam@gmail.com>
2020-12-24 10:49:08 +0530

    create custom post type 'book'.

--- wpeka-wp-book.php
+++ wpeka-wp-book.php
@@ -5,3 +5,9 @@ Description: This is Wpeka plugin development assignment - book plugin.
 Author: Vikram Kadam
 version: 2.1.1
 */
+
+
+/*******************************
+includes
+********************************/
+include('includes/custom-post-type.php');
