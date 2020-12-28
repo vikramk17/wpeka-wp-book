877f242582ab4685e408e36c1efa419b79b5e74e
vikramk17 <vickramskadam@gmail.com>
2020-12-28 11:07:04 +0530

    create custom meta table 'book-meta' in db.

--- includes/custom-meta-table.php
+++ includes/custom-meta-table.php
@@ -1,25 +1,4 @@
 <?php
-/*
-plugin name: WP book
-Description: This is Wpeka plugin development assignment - book plugin.
-Author: Vikram Kadam
-version: 2.1.1
-*/
-
-
-/*******************************
-includes
-********************************/
-// include('includes/custom-meta-table.php');
-include('includes/custom-post-type.php');
-include('includes/custom-taxonomy.php');
-include('includes/custom-metabox.php');
-include('includes/settings.php');
-
-
-
-
-// function to add custom table to database
 
 function wpb_book_meta_install() {
 
