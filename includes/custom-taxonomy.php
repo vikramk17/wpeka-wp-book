6b7eb322e860861f717ca37a161f73ef1cd07bac
vikramk17 <vickramskadam@gmail.com>
2021-01-04 09:09:44 +0530

    change the label array of book tag and book category

--- includes/custom-taxonomy.php
+++ includes/custom-taxonomy.php
@@ -3,17 +3,17 @@
 
 function wpb_register_taxonomy_book_category() {
      $labels = array(
-         'name'              => _x( 'Book Category', 'taxonomy general name' ),
-         'singular_name'     => _x( 'Book Category', 'taxonomy singular name' ),
-         'search_items'      => __( 'Search Book Category' ),
-         'all_items'         => __( 'All Book Category' ),
-         'parent_item'       => __( 'Parent Book Category' ),
-         'parent_item_colon' => __( 'Parent Book Category:' ),
-         'edit_item'         => __( 'Edit Book Category' ),
-         'update_item'       => __( 'Update Book Category' ),
-         'add_new_item'      => __( 'Add New Book Category' ),
-         'new_item_name'     => __( 'New Book Category Name' ),
-         'menu_name'         => __( 'Book Category' ),
+         'name'              => _x( 'Book Category', 'wp-book' ),
+         'singular_name'     => _x( 'Book Category', 'wp-book' ),
+         'search_items'      => __( 'Search Book Category', 'wp-book' ),
+         'all_items'         => __( 'All Book Category', 'wp-book' ),
+         'parent_item'       => __( 'Parent Book Category', 'wp-book' ),
+         'parent_item_colon' => __( 'Parent Book Category:', 'wp-book' ),
+         'edit_item'         => __( 'Edit Book Category', 'wp-book' ),
+         'update_item'       => __( 'Update Book Category', 'wp-book' ),
+         'add_new_item'      => __( 'Add New Book Category', 'wp-book' ),
+         'new_item_name'     => __( 'New Book Category Name', 'wp-book' ),
+         'menu_name'         => __( 'Book Category', 'wp-book' ),
      );
      $args   = array(
          'hierarchical'      => true,
@@ -33,17 +33,17 @@ add_action( 'init', 'wpb_register_taxonomy_book_category' );
 
 function wpb_register_taxonomy_book_tag() {
      $labels = array(
-         'name'              => _x( 'Book Tag', 'taxonomy general name' ),
-         'singular_name'     => _x( 'Book Tag', 'taxonomy singular name' ),
-         'search_items'      => __( 'Search Book Tag' ),
-         'all_items'         => __( 'All Book Tag' ),
-         'parent_item'       => __( 'Parent Book Tag' ),
-         'parent_item_colon' => __( 'Parent Book Tag:' ),
-         'edit_item'         => __( 'Edit Book Tag' ),
-         'update_item'       => __( 'Update Book Tag' ),
-         'add_new_item'      => __( 'Add New Book Tag' ),
-         'new_item_name'     => __( 'New Book Tag Name' ),
-         'menu_name'         => __( 'Book Tag' ),
+         'name'              => _x( 'Book Tag', 'wp-book' ),
+         'singular_name'     => _x( 'Book Tag', 'wp-book' ),
+         'search_items'      => __( 'Search Book Tag', 'wp-book' ),
+         'all_items'         => __( 'All Book Tag', 'wp-book' ),
+         'parent_item'       => __( 'Parent Book Tag', 'wp-book' ),
+         'parent_item_colon' => __( 'Parent Book Tag:', 'wp-book' ),
+         'edit_item'         => __( 'Edit Book Tag', 'wp-book' ),
+         'add_new_item'      => __( 'Add New Book Tag', 'wp-book' ),
+         'update_item'       => __( 'Update Book Tag', 'wp-book' ),
+         'new_item_name'     => __( 'New Book Tag Name', 'wp-book' ),
+         'menu_name'         => __( 'Book Tag', 'wp-book' ),
      );
      $args   = array(
          'hierarchical'      => false,
