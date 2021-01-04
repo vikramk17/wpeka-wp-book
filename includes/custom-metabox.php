916eb99e214e3fedade7ed4f9102f5f75c93bc0a
vikramk17 <vickramskadam@gmail.com>
2021-01-04 09:07:11 +0530

    Getting metabox values and saving the values to the custom database.

--- includes/custom-metabox.php
+++ includes/custom-metabox.php
@@ -3,43 +3,51 @@
 //creating a custom meta box
 
 function wpb_custom_meta_box_markup( $post ) {
-    wp_nonce_field(basename(__FILE__), "wpb_meta-box-nonce");
+
+    //getting metabox values from database
+    $wpb_author_name = get_metadata( 'book', $post->ID, 'author-name', $single=true );
+    $wpb_price = get_metadata( 'book', $post->ID, 'price', $single = true );
+    $wpb_publisher = get_metadata( 'book', $post->ID, 'publisher', $single = true );
+    $wpb_year = get_metadata( 'book', $post->ID, 'year', $single = true );
+    $wpb_edition = get_metadata( 'book', $post->ID, 'edition', $single = true );
+    $wpb_url = get_metadata( 'book', $post->ID, 'url', $single = true );
     ?>
     <div class="wpb_box">
         <p class="meta-options wpb_field">
           <span>
             <label for="wpb_author_name">Author name:</label>
             <input type="text" name="wpb_author_name" id="wpb_author_name"
-            value="<?php echo esc_attr( get_post_meta( get_the_ID(), 'wpb_author_name', true ) ); ?>">
+            value="<?php echo esc_attr( $wpb_author_name ); ?>">
           </span>
           <span>
             <label for="wpb_book_price">Price:</label>
             <input type="number" name=wpb_book_price" id="wpb_book_price"
-            value="<?php echo esc_attr( get_post_meta( get_the_ID(), 'wpb_book_price', true ) ); ?>">
+            value="<?php echo esc_attr( $wpb_price ); ?>">
           </span>
         </p>
         <p class="meta-options wpb_field">
           <span>
             <label for="wpb_book_publisher">Publisher:</label>
             <input type="text" name="wpb_book_publisher" id="wpb_book_publisher"
-            value="<?php echo esc_attr( get_post_meta( get_the_ID(), 'wpb_book_publisher', true ) ); ?>">
+            value="<?php echo esc_attr( $wpb_publisher ); ?>">
           </span>
           <span>
             <label for="wpb_book_publish_year">Published Year:</label>
             <input type="month" name="wpb_book_publish_year" id="wpb_book_publish_year"
-            value="<?php echo esc_attr( get_post_meta( get_the_ID(), 'wpb_book_publish_year', true ) ); ?>">
+            value="<?php echo esc_attr( $wpb_year ); ?>">
           </span>
           <span>
             <label for="wpb_book_edition">Edition:</label>
             <input type="text" name="wpb_book_edition" id="wpb_book_edition"
-            value="<?php echo esc_attr( get_post_meta( get_the_ID(), 'wpb_book_edition', true ) ); ?>">
+            value="<?php echo esc_attr( $wpb_edition ); ?>">
           </span>
         </p>
         <p class="meta-options wpb_field">
           <label for="wpb_book_url">Book URL:</label>
           <input type="url" name="wpb_book_url" id="wpb_book_url"
-          value="<?php echo esc_attr( get_post_meta( get_the_ID(), 'wpb_book_url', true ) ); ?>">
+          value="<?php echo esc_attr( $wpb_url ); ?>">
         </p>
+        <?php wp_nonce_field( 'wpb_custom_book_info_nonce', 'wpb_book_info_nonce' ); ?>
     </div>
     <?php
 }
@@ -53,32 +61,35 @@ add_action( "add_meta_boxes", "wpb_add_custom_meta_box" );
 
 //save custom meta box
 
-function wpb_save_custom_meta_box( $post_id ) {
-     //check if the post is auto saving
-     if( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) return;
+function wpb_custom_book_info_nonce( $post_id ){
 
-     if ( $parent_id = wp_is_post_revision( $post_id ) ) {
-       $post_id = $parent_id;
-     }
-     //verify the nonce value we created earlier
-     if (!isset($_POST["wpb_meta-box-nonce"]) || !wp_verify_nonce($_POST["wpb_meta-box-nonce"], basename(__FILE__)))
-       return;
-    //check to make sure the current user can actually edit the post
-     if( !current_user_can( 'edit_post' ) ) return;
+    if( ! isset( $_POST['wpb_book_info_nonce'] ) || ! wp_verify_nonce( $_POST['wpb_book_info_nonce'], 'wpb_custom_book_info_nonce' ) ){
+        return $post_id;
+    }
 
-     $fields = [
-         'wpb_author_name',
-         'wpb_book_price',
-         'wpb_book_publisher',
-         'wpb_book_publish_year',
-         'wpb_book_edition',
-         'wpb_book_url',
-     ];
-     foreach ( $fields as $field ) {
-         if ( array_key_exists( $field, $_POST ) ) {
-             // update_post_meta( $post_id, $field, sanitize_text_field( $_POST[$field] ) );
-             update_book_meta( $post_id, $field, sanitize_text_field( $_POST[$field] ) );
-         }
-     }
+    if( isset( $_POST['author_name'] ) ){
+        update_metadata('book', $post_id, 'author-name', sanitize_text_field($_POST['author_name']) );
+    }
+
+    if( isset( $_POST['price'] ) ){
+        update_metadata('book', $post_id, 'price', sanitize_text_field($_POST['price']) );
+    }
+
+    if( isset( $_POST['publisher'] ) ){
+        update_metadata('book', $post_id, 'publisher', sanitize_text_field($_POST['publisher']) );
+    }
+
+    if( isset( $_POST['year'] ) ){
+        update_metadata('book', $post_id, 'year', sanitize_text_field($_POST['year']) );
+    }
+
+    if( isset( $_POST['edition'] ) ){
+        update_metadata('book', $post_id, 'edition', sanitize_text_field($_POST['edition']) );
+    }
+
+    if( isset( $_POST['url'] ) ){
+        update_metadata('book', $post_id, 'url', sanitize_text_field($_POST['url']) );
+    }
+
 }
-add_action( "save_post", "wpb_save_custom_meta_box" );
+add_action( "save_post", "wpb_custom_book_info_nonce" );
